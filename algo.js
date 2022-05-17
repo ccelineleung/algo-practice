64bfc2edc69a2d4aa92af4fc4054017b6b4d86d6
Celine Leung <celineleung412@gmail.com>
2022-05-16 23:47:39 -0700

    update

--- algo.js
+++ algo.js
@@ -867,13 +867,13 @@ function intersection(arrays) {
   console.log(firstResult);
 }
 
-console.log(
-  intersection([
-    [5, 10, 15, 20],
-    [15, 88, 1, 5, 7],
-    [1, 10, 15, 5, 20],
-  ])
-);
+// console.log(
+//   intersection([
+//     [5, 10, 15, 20],
+//     [15, 88, 1, 5, 7],
+//     [1, 10, 15, 5, 20],
+//   ])
+// );
 //should log: [5, 15]
 
 // Challenge 8h
