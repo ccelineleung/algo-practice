dd4505ec1ade68ee4505631aab70ec8cbb536085
Celine Leung <celineleung412@gmail.com>
2022-06-07 01:44:25 -0700

    practices for stack and queue

--- LinkedList.js
+++ LinkedList.js
@@ -1,128 +1,203 @@
-class Node {
-  constructor(val) {
-    this.val = val;
-    this.next = null;
-  }
-}
+// class Node {
+//   constructor(val) {
+//     this.val = val;
+//     this.next = null;
+//   }
+// }
 
-class SinglyLinkedList {
-  constructor() {
-    this.head = null;
-    this.tail = null;
-    this.length = 0;
-  }
-  push(val) {
-    let newNode = new Node(val);
-    if (!this.head) {
-      this.head = newNode;
-      this.tail = this.head;
-    } else {
-      this.tail.next = newNode;
-      this.tail = newNode;
-    }
-    this.length++;
-    return this;
-  }
-  pop() {
-    if (this.length === undefined) return undefined;
-    let current = this.head;
-    let newTail = current;
-    while (current.next) {
-      newTail = current;
-      current = current.next;
-    }
-    this.tail = newTail;
-    this.tail.next = null;
-    this.length--;
-    if (this.length === 0) {
-      this.head = null;
-      this.tail = null;
-    }
-    return current;
-  }
-  shift() {
-    if (!this.head) return undefined;
-    let current = this.head;
-    this.head = current.next;
-    this.length--;
-    if (this.length === 0) {
-      this.tail = null;
-    }
-    return current;
-  }
-  unshift(val) {
-    let newNode = new Node(val);
-    if (!this.head) {
-      this.head = newNode;
-      this.tail = newNode;
-    } else {
-      newNode.next = this.head;
-      this.head = newNode;
-    }
-    this.length++;
-    return this;
-  }
-  get(index) {
-    if (index < 0 || index >= this.length) return null;
-    let count = 0;
-    let current = this.head;
-    while (count !== index) {
-      current = this.head.next;
-      count++;
-    }
-    return current;
-  }
-  set(value, index) {
-    let foundNode = this.get(index);
-    if (!foundNode) {
-      return false;
-    } else {
-      foundNode.value = value;
-      return true;
-    }
-  }
-  insert(index, val) {
-    if (index < 0 || index > this.length) {
-      return false;
-    } else if (index === this.length) {
-      return this.push(val);
-    } else if (index === 0) {
-      this.unshift(val);
-    } else {
-      let newNode = new Node(val);
-      let prev = this.get(index - 1);
-      let temp = prev.next;
-      prev.next = newNode;
-      newNode.next = temp;
-      this.length++;
-      return true;
-    }
-  }
-  remove(index) {
-    if (index < 0 || index > this.length) return undefined;
-    if (index === this.length - 1) return this.pop();
-    if (index === 0) return this.shift();
-    let prevNode = this.get(index - 1);
-    let remove = prevNode.next;
-    prevNode.next = remove.next;
-    this.length--;
-    return remove;
-  }
-  reverse() {
-    let node = this.head;
-    this.head = this.tail;
-    this.tail = node;
-    let next;
-    let prev = null;
+// class SinglyLinkedList {
+//   constructor() {
+//     this.head = null;
+//     this.tail = null;
+//     this.length = 0;
+//   }
+//   push(val) {
+//     let newNode = new Node(val);
+//     if (!this.head) {
+//       this.head = newNode;
+//       this.tail = this.head;
+//     } else {
+//       this.tail.next = newNode;
+//       this.tail = newNode;
+//     }
+//     this.length++;
+//     return this;
+//   }
+//   pop() {
+//     if (this.length === undefined) return undefined;
+//     let current = this.head;
+//     let newTail = current;
+//     while (current.next) {
+//       newTail = current;
+//       current = current.next;
+//     }
+//     this.tail = newTail;
+//     this.tail.next = null;
+//     this.length--;
+//     if (this.length === 0) {
+//       this.head = null;
+//       this.tail = null;
+//     }
+//     return current;
+//   }
+//   shift() {
+//     if (!this.head) return undefined;
+//     let current = this.head;
+//     this.head = current.next;
+//     this.length--;
+//     if (this.length === 0) {
+//       this.tail = null;
+//     }
+//     return current;
+//   }
+//   unshift(val) {
+//     let newNode = new Node(val);
+//     if (!this.head) {
+//       this.head = newNode;
+//       this.tail = newNode;
+//     } else {
+//       newNode.next = this.head;
+//       this.head = newNode;
+//     }
+//     this.length++;
+//     return this;
+//   }
+//   get(index) {
+//     if (index < 0 || index >= this.length) return null;
+//     let count = 0;
+//     let current = this.head;
+//     while (count !== index) {
+//       current = this.head.next;
+//       count++;
+//     }
+//     return current;
+//   }
+//   set(value, index) {
+//     let foundNode = this.get(index);
+//     if (!foundNode) {
+//       return false;
+//     } else {
+//       foundNode.value = value;
+//       return true;
+//     }
+//   }
+//   insert(index, val) {
+//     if (index < 0 || index > this.length) {
+//       return false;
+//     } else if (index === this.length) {
+//       return this.push(val);
+//     } else if (index === 0) {
+//       this.unshift(val);
+//     } else {
+//       let newNode = new Node(val);
+//       let prev = this.get(index - 1);
+//       let temp = prev.next;
+//       prev.next = newNode;
+//       newNode.next = temp;
+//       this.length++;
+//       return true;
+//     }
+//   }
+//   remove(index) {
+//     if (index < 0 || index > this.length) return undefined;
+//     if (index === this.length - 1) return this.pop();
+//     if (index === 0) return this.shift();
+//     let prevNode = this.get(index - 1);
+//     let remove = prevNode.next;
+//     prevNode.next = remove.next;
+//     this.length--;
+//     return remove;
+//   }
+//   reverse() {
+//     let node = this.head;
+//     this.head = this.tail;
+//     this.tail = node;
+//     let next;
+//     let prev = null;
 
-    for (let i = 0; i < this.length; i++) {
-      next = node.next;
-      node.next = prev;
-      prev = node;
-      node = next;
-    }
-    return this;
-  }
-}
-let list = new SinglyLinkedList();
-list.push("HELLO");
+//     for (let i = 0; i < this.length; i++) {
+//       next = node.next;
+//       node.next = prev;
+//       prev = node;
+//       node = next;
+//     }
+//     return this;
+//   }
+// }
+
+//-----------------------------------------------------BUILD STACK-------------------
+// class Node {
+//   constructor(value) {
+//     this.value = value;
+//     this.next = null;
+//   }
+// }
+// class Stack {
+//   constructor() {
+//     this.first = null;
+//     this.last = null;
+//     this.size = 0;
+//   }
+//   push(val) {
+//     let newNode = new Node(val);
+//     if (!this.first) {
+//       this.first = newNode;
+//       this.last = newNode;
+//     } else {
+//       let temp = this.first;
+//       this.first = newNode;
+//       this.first.next = temp;
+//     }
+//     return ++this.size;
+//   }
+//   pop(){
+//     if(!this.first) return null;
+//     let temp = this.first;
+//     if (this.first === this.last){
+//       this.last = null;
+//     }
+//     this.first = this.first.next;
+//     this.size--;
+//     return temp.value;
+//   }
+// }
+
+//-----------------------------------------------------BUILD QUEUE-------------------
+//often use insertion and removal , because of O(1)
+
+// class Node {
+//   constructor(value) {
+//     this.value = value;
+//     this.next = null;
+//   }
+// }
+// class Queue {
+//   constructor() {
+//     this.first = null;
+//     this.last = null;
+//     this.size = 0;
+//   }
+//   enqueue(val) {
+//     let newNode = new Node(val);
+//     if (!this.first) {
+//       this.first = newNode;
+//       this.last = newNode;
+//     } else {
+//       this.last.next = newNode;
+//       this.last = newNode;
+//     }
+//     return ++this.size;
+//   }
+//   dequeue() {
+//     if (!this.first) return null;
+//     let temp = this.first;
+//     if (this.first === this.last) {
+//       this.last = null;
+//     }
+//     this.first = this.first.next;
+//     this.size--;
+//     return temp.value;
+//   }
+// }
+
+//----------------------------Trees, BINARY TREES, BINARY SEARCH TREES-----------------------------------
