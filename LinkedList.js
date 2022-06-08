b97f248e099d435a6095a02dcc0eae8a800c8716
Celine Leung <celineleung412@gmail.com>
2022-06-08 00:10:01 -0700

    BINARY SEARCH TREE CLASS

--- LinkedList.js
+++ LinkedList.js
@@ -200,4 +200,65 @@
 //   }
 // }
 
-//----------------------------Trees, BINARY TREES, BINARY SEARCH TREES-----------------------------------
+//---------------------------- BINARY SEARCH TREE CLASS-----------------------------------
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+  insert(value) {
+    let newNode = new Node(value);
+    if (!this.root) {
+      this.root = newNode;
+      return this;
+    } else {
+      let current = this.root;
+      while (true) {
+        if (value === current.value) return undefined;
+        if (value < current.value) {
+          if (current.left === null) {
+            current.left = newNode;
+            return this;
+          } else {
+            current = current.left;
+          }
+        } else if (value > current.value) {
+          if (current.right === null) {
+            current.right = newNode;
+            return this;
+          } else {
+            current = current.right;
+          }
+        }
+      }
+    }
+  }
+  find(value) {
+    if (!this.root) return false;
+    let current = this.root,
+      found = false;
+    while (current && !found) {
+      if (value < current.value) {
+        current = current.left;
+      } else if (value > current.value) {
+        current = current.right;
+      } else {
+        found = true;
+      }
+    }
+    if (!found) return undefined;
+    return current;
+  }
+}
+let tree = new BinarySearchTree();
+console.log(tree.insert(10));
+console.log(tree.insert(5));
+console.log(tree.insert(13));
