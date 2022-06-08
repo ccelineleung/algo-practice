3aa01994583105867cbc593acf73c58e18948440
Celine Leung <celineleung412@gmail.com>
2022-06-08 16:44:00 -0700

    Tree Problems

--- LinkedList.js
+++ LinkedList.js
@@ -257,8 +257,59 @@ class BinarySearchTree {
     if (!found) return undefined;
     return current;
   }
+  breadthFirstSearch() {
+    let node = this.root;
+    let data = [];
+    let queue = [];
+    queue.push(node);
+
+    while (queue.length) {
+      node = queue.shift();
+      data.push(node.value);
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+    }
+    return data;
+  }
+  DFSPreOrder() {
+    let data = [];
+    function traverse(node) {
+      data.push(node.value);
+      if (node.left) traverse(node.left);
+      if (node.right) traverse(node.right);
+    }
+    traverse(this.root);
+    return data;
+  }
+  DFSPostOrder() {
+    let data = [];
+    function traverse(node) {
+      if (node.left) traverse(node.left);
+      if (node.right) traverse(node.right);
+      data.push(node.value);
+    }
+    traverse(this.root);
+    return data;
+  }
+  DFSInOrder() {
+    let data = [];
+    function traverse(node) {
+      if (node.left) traverse(node.left);
+      data.push(node.value);
+      if (node.right) traverse(node.right);
+    }
+    traverse(this.root);
+    return data;
+  }
 }
 let tree = new BinarySearchTree();
-console.log(tree.insert(10));
-console.log(tree.insert(5));
-console.log(tree.insert(13));
+tree.insert(10);
+tree.insert(6);
+tree.insert(15);
+tree.insert(3);
+tree.insert(8);
+tree.insert(20);
+console.log(tree.breadthFirstSearch());
+console.log(tree.DFSPreOrder());
+console.log(tree.DFSPostOrder());
+console.log(tree.DFSInOrder());
