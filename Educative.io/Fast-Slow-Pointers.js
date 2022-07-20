450924549ec82ad997b05ae31a278f087e2ed914
Celine Leung <celineleung412@gmail.com>
2022-07-20 15:00:23 -0700

    Jul20

--- Educative.io/Fast-Slow-Pointers.js
+++ Educative.io/Fast-Slow-Pointers.js
@@ -267,14 +267,52 @@ const reorder = function (head) {
 
 //------------------------Cycle in a Circular Array Leetcode # 457. Circular Array Loop --------------------------//
 
+const circular_array_loop_exists = function (arr) {
+  for (let i = 0; i < arr.length; i++) {
+    isForward = arr[i] >= 0;
+    let slow = i;
+    let fast = i;
+    console.log(slow, fast);
 
+    while (true) {
+      slow = findNextIndex(arr, isForward, slow);
+      fast = findNextIndex(arr, isForward, fast);
+      console.log(slow, fast);
+      if (fast !== -1) {
+        fast = findNextIndex(arr, isForward, fast);
+      }
+      console.log(slow, fast);
+      if (slow === -1 || fast === -1 || slow === fast) {
+        break;
+      }
+    }
 
-const circular_array_loop_exists = function(arr) {
-    
+    if (slow !== -1 && slow === fast) {
+      return true;
+    }
+  }
+  return false;
 };
 
+function findNextIndex(arr, isForward, currentIndex) {
+  let direction = arr[currentIndex] >= 0;
+  console.log(direction);
+  if (isForward !== direction) return -1; //change the direction , return -1;
 
+  let nextIndex = (currentIndex + arr[currentIndex]) % arr.length;
+  console.log(nextIndex);
+  if (nextIndex < 0) {
+    nextIndex += arr.length; //wrap around for negative numbers
+  }
 
-console.log(`${circular_array_loop_exists([1, 2, -1, 2, 2])}`)
-console.log(`${circular_array_loop_exists([2, 2, -1, 2])}`)
-console.log(`${circular_array_loop_exists([2, 1, -1, -2])}`)
+  if (nextIndex === currentIndex) {
+    //one element cycle, return -1
+    nextIndex = -1;
+  }
+
+  return nextIndex;
+}
+
+//console.log(`${circular_array_loop_exists([1, 2, -1, 2, 2])}`);
+// console.log(`${circular_array_loop_exists([2, 2, -1, 2])}`)
+// console.log(`${circular_array_loop_exists([2, 1, -1, -2])}`);
