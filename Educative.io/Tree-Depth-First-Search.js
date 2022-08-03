b16e3fb5b3b6500ebfd4a8c7dec7f4403bb80d0d
Celine Leung <celineleung412@gmail.com>
2022-08-02 22:53:35 -0700

    Aug2

--- Educative.io/Tree-Depth-First-Search.js
+++ Educative.io/Tree-Depth-First-Search.js
@@ -71,11 +71,11 @@ const find_sum_of_path_numbers = function (root) {
     if (node.left === null && node.right === null) {
       total += sum;
     }
-    if(node.left) dfs(node.left, sum) 
-    if(node.right) dfs(node.right, sum);
+    if (node.left) dfs(node.left, sum);
+    if (node.right) dfs(node.right, sum);
   };
-  dfs(root)
-  return total
+  dfs(root);
+  return total;
 };
 
 // var root = new TreeNode(1);
@@ -88,31 +88,146 @@ const find_sum_of_path_numbers = function (root) {
 
 //------------------------------Path With Given Sequence Leetcode # ---------------------//
 
-const find_path = function(root, sequence) {
-    if( !root ) return sequence.length === 0;
+const find_path = function (root, sequence) {
+  if (!root) return sequence.length === 0;
 
-    const seqLength = sequence.length;
+  const seqLength = sequence.length;
 
-    const dfs = (node, index = 0) => {
-        if (!node) return false;
+  const dfs = (node, index = 0) => {
+    if (!node) return false;
 
-        if (index >= seqLength || node.value !== sequence[index]) return false;
+    if (index >= seqLength || node.value !== sequence[index]) return false;
 
-        if(node.left === null && node.right === null && index === seqLength - 1) return true;
+    if (node.left === null && node.right === null && index === seqLength - 1)
+      return true;
 
-        return dfs(node.left, index + 1) || dfs (node.right, index + 1)
-    }
-    return dfs(root)
+    return dfs(node.left, index + 1) || dfs(node.right, index + 1);
   };
-  
+  return dfs(root);
+};
+
+//   var root = new TreeNode(1)
+//   root.left = new TreeNode(0)
+//   root.right = new TreeNode(1)
+//   root.left.left = new TreeNode(1)
+//   root.right.left = new TreeNode(6)
+//   root.right.right = new TreeNode(5)
+
+//   console.log(`Tree has path sequence: ${find_path(root, [1, 0, 7])}`)
+//   console.log(`Tree has path sequence: ${find_path(root, [1, 1, 6])}`)
+
+//------------------------------Count Paths for a Sum Leetcode # 437. Path Sum III---------------------//
+
+const count_paths = function (root, S) {
+  function countPathsRecursive(currNode, S, currPath = []) {
+    if (currNode === null) return 0;
+
+    currPath.push(currNode.value);
+
+    let pathCount = 0;
+    let pathSum = 0;
+
+    for (i = currPath.length - 1; i > 0; i--) {
+      pathSum += currPath[i];
+
+      if (pathSum === S) {
+        pathCount += 1;
+      }
+    }
+
+    pathCount += countPathsRecursive(currNode.left, S, currPath);
+    pathCount += countPathsRecursive(currNode.right, S, currPath);
+
+    //console.log(currPath)
+    currPath.pop();
+    return pathCount;
+  }
+
+  return countPathsRecursive(root, S);
+};
+
+// var root = new TreeNode(12);
+// root.left = new TreeNode(7);
+// root.right = new TreeNode(1);
+// root.left.left = new TreeNode(4);
+// root.right.left = new TreeNode(10);
+// root.right.right = new TreeNode(5);
+// console.log(`Tree has paths: ${count_paths(root, 11)}`);
+
+//------------------------------Tree Diameter Leetcode # 543. Diameter of Binary Tree---------------------//
+
+function find_diameter(root) {
+  if (!root) return 0;
+  let diameter = 0;
+
+  function helper(node) {
+    if (!node) return 0;
+    //console.log(node.value);
+
+    const leftPath = helper(node.left);
+    //console.log(leftPath)
+    const rightPath = helper(node.right);
+    //console.log(rightPath)
+
+    diameter = Math.max(diameter, leftPath + rightPath);
+    return Math.max(leftPath, rightPath) + 1;
+  }
+  helper(root);
+  return diameter;
+}
+
+// const root = new TreeNode(1);
+// root.left = new TreeNode(2);
+// root.right = new TreeNode(3);
+// root.left.left = new TreeNode(4);
+// root.right.left = new TreeNode(5);
+// root.right.right = new TreeNode(6);
+// console.log(find_diameter(root));
+// root.left.left = null;
+// root.right.left.left = new TreeNode(7);
+// root.right.left.right = new TreeNode(8);
+// root.right.right.left = new TreeNode(9);
+// root.right.left.right.left = new TreeNode(10);
+// root.right.right.left.left = new TreeNode(11);
+// console.log(find_diameter(root));
+
+//------------------------------Path with Maximum Sum Leetcode # 124. Binary Tree Maximum Path Sum---------------------//
+
+
+function find_maximum_path_sum(root) {
+       let largest = -Infinity;
+
+       function dfs(node) {
+        if (!node) return 0;
+
+        let leftSum = dfs(node.left);
+        let rightSum = dfs(node.right);
+
+        largest = Math.max(largest, node.value+leftSum+rightSum);
+        return Math.max(0, node.value + leftSum, node.value+ rightSum)
+       }
+
+       dfs(root)
+       return largest
+    }
+
+
   
   
   var root = new TreeNode(1)
-  root.left = new TreeNode(0)
-  root.right = new TreeNode(1)
-  root.left.left = new TreeNode(1)
-  root.right.left = new TreeNode(6)
-  root.right.right = new TreeNode(5)
+  root.left = new TreeNode(2)
+  root.right = new TreeNode(3)
+  console.log(`Maximum Path Sum: ${find_maximum_path_sum(root)}`)
   
-  console.log(`Tree has path sequence: ${find_path(root, [1, 0, 7])}`)
-  console.log(`Tree has path sequence: ${find_path(root, [1, 1, 6])}`)
+  root.left.left = new TreeNode(1)
+  root.left.right = new TreeNode(3)
+  root.right.left = new TreeNode(5)
+  root.right.right = new TreeNode(6)
+  root.right.left.left = new TreeNode(7)
+  root.right.left.right = new TreeNode(8)
+  root.right.right.left = new TreeNode(9)
+  console.log(`Maximum Path Sum: ${find_maximum_path_sum(root)}`)
+  
+  root = new TreeNode(-1)
+  root.left = new TreeNode(-3)
+  console.log(`Maximum Path Sum: ${find_maximum_path_sum(root)}`)
