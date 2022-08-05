132247441a8e1f1d991c8645a2b9a3a8b8448b4b
Celine Leung <celineleung412@gmail.com>
2022-08-05 01:08:10 -0700

    Aug5

--- Educative.io/Modified-Binary-Search.js
+++ Educative.io/Modified-Binary-Search.js
@@ -226,10 +226,17 @@ const find_max_in_bitonic_array = function(arr) {
     let start = 0;
     let end = arr.length - 1;
 
-    while (start <= end) {
-        
-    }
+    while (start < end) {
+        let mid = Math.floor((start+end)/2);
 
+        if (arr[mid] <= arr[mid+1]) {
+            start = mid + 1;
+        } else if (arr[mid] > arr[mid+1]){
+            end = mid
+        }
+    }
+    // at the end of the while loop, 'start === end'
+    return arr[start]
   };
   
   
