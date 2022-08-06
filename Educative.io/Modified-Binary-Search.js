b22ae47b9ccabad9726c67b4d9461cdf66be0905
Celine Leung <celineleung412@gmail.com>
2022-08-06 00:46:38 -0700

    Aug6

--- Educative.io/Modified-Binary-Search.js
+++ Educative.io/Modified-Binary-Search.js
@@ -222,26 +222,137 @@ const search_min_diff_element = function (arr, key) {
 /*Find the maximum value in a given Bitonic array. An array is considered bitonic if it is monotonically increasing and then monotonically decreasing. 
 Monotonically increasing or decreasing means that for any index i in the array arr[i] != arr[i+1].*/
 
-const find_max_in_bitonic_array = function(arr) {
-    let start = 0;
-    let end = arr.length - 1;
+const find_max_in_bitonic_array = function (arr) {
+  let start = 0;
+  let end = arr.length - 1;
 
-    while (start < end) {
-        let mid = Math.floor((start+end)/2);
+  while (start < end) {
+    let mid = Math.floor((start + end) / 2);
 
-        if (arr[mid] <= arr[mid+1]) {
-            start = mid + 1;
-        } else if (arr[mid] > arr[mid+1]){
-            end = mid
-        }
+    if (arr[mid] <= arr[mid + 1]) {
+      start = mid + 1;
+    } else if (arr[mid] > arr[mid + 1]) {
+      end = mid;
     }
-    // at the end of the while loop, 'start === end'
-    return arr[start]
+  }
+  // at the end of the while loop, 'start === end'
+  return arr[start];
+};
+
+//   console.log(find_max_in_bitonic_array([1, 3, 8, 12, 4, 2]))
+//   console.log(find_max_in_bitonic_array([3, 8, 3, 1]))
+//   console.log(find_max_in_bitonic_array([1, 3, 8, 12]))
+//   console.log(find_max_in_bitonic_array([10, 9, 8]))
+
+//-----------------------------------Search Bitonic Array LeetCode #-----------------------//
+/*Given a Bitonic array, find if a given ‘key’ is present in it. An array is considered bitonic if it is monotonically increasing and then monotonically 
+decreasing. Monotonically increasing or decreasing means that for any index i in the array arr[i] != arr[i+1].
+
+Write a function to return the index of the ‘key’. If the ‘key’ is not present, return -1. */
+const search_bitonic_array = function (arr, key) {
+  const maxIndex = findMax(arr);
+  const keyIndex = binarySearch(arr, key, 0, maxIndex);
+  if (keyIndex !== -1) {
+    return keyIndex;
+  }
+  return binarySearch(arr, key, maxIndex + 1, arr.length - 1);
+};
+
+function findMax(arr) {
+  let start = 0;
+  let end = arr.length - 1;
+  let maxIndex = 0;
+  while (start < end) {
+    let mid = Math.floor((start + end) / 2);
+
+    if (arr[mid] > arr[mid + 1]) {
+      end = mid;
+    } else if (arr[mid] <= arr[mid + 1]) {
+      start = mid + 1;
+    }
+  }
+  return start;
+}
+
+const binarySearch = function (arr, key, start, end) {
+  isAscending = arr[start] < arr[end];
+
+  while (start <= end) {
+    let mid = Math.floor((start + end) / 2);
+
+    if (key === arr[mid]) return mid;
+
+    if (arr[start] < arr[end]) {
+      if (key < arr[mid]) {
+        end = mid - 1;
+      } else {
+        start = mid + 1;
+      }
+    } else {
+      if (key < arr[mid]) {
+        start = mid + 1;
+      } else {
+        end = mid - 1;
+      }
+    }
+  }
+  return -1;
+};
+
+// console.log(search_bitonic_array([1, 3, 8, 4, 3], 4));
+// console.log(search_bitonic_array([3, 8, 3, 1], 8));
+// console.log(search_bitonic_array([1, 3, 8, 12], 12));
+// console.log(search_bitonic_array([10, 9, 8], 10));
+
+//-----------------------------------Search in Rotated Array LeetCode #33. Search in Rotated Sorted Array-----------------------//
+/*Given an array of numbers which is sorted in ascending order and also rotated by some arbitrary number, find if a given ‘key’ is present in it.
+
+Write a function to return the index of the ‘key’ in the rotated array. If the ‘key’ is not present, return -1. You can assume that the given 
+array does not have any duplicates. */
+
+const search_rotated_array = function (arr, key) {
+  let start = 0;
+  let end = arr.length - 1;
+  let maxIndex = -Infinity;
+
+  while (start <= end) {
+    let mid = Math.floor((start + end) / 2);
+    if (arr[mid] === key) return mid;
+
+    if (arr[start] <= arr[mid]) {
+      //left side is sorted in ascending order
+      if (key >= arr[start] && key < arr[mid]) {
+        end = mid - 1;
+      } else {
+        //key > arr[mid]
+        start = mid + 1;
+      }
+    } else {
+      //right side is sorted in ascending order
+      if (key > arr[mid] && key <= arr[end]) {
+        start = mid + 1;
+      } else {
+        end = mid - 1;
+      }
+    }
+  }
+  return -1;
+};
+
+// console.log(search_rotated_array([10, 15, 1, 3, 8], 15));
+// console.log(search_rotated_array([4, 5, 7, 9, 10, -1, 2], 10));
+
+//-----------------------------------Rotation Count LeetCode #153. Find Minimum in Rotated Sorted Array-----------------------//
+/*Given an array of numbers which is sorted in ascending order and is rotated ‘k’ times around a pivot, find ‘k’.
+
+You can assume that the array does not have any duplicates. */
+
+const count_rotations = function(arr) {
+    // TODO: Write your code here
+    return -1 ;
   };
   
   
-  console.log(find_max_in_bitonic_array([1, 3, 8, 12, 4, 2]))
-  console.log(find_max_in_bitonic_array([3, 8, 3, 1]))
-  console.log(find_max_in_bitonic_array([1, 3, 8, 12]))
-  console.log(find_max_in_bitonic_array([10, 9, 8]))
-  
+  console.log(count_rotations([10, 15, 1, 3, 8]))
+  console.log(count_rotations([4, 5, 7, 9, 10, -1, 2]))
+  console.log(count_rotations([1, 3, 8, 10]))
