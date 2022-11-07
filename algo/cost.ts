19e22ba2e70b2428f9b3d49600aea4758daf2da3
Celine <celineleung412@gmail.com>
2022-11-07 11:37:03 -0800

    updated with sudo code

--- algo/cost.ts
+++ algo/cost.ts
@@ -42,105 +42,86 @@ const testStr = `{
     //     }
       
     //     `;
-  const calcCost2 = (str) => {
-    let firstBracket, lastBracket;
-    let fragsObj = {};
   
-    let query = str.slice(0, str.indexOf("fragment"));
-    let fragments = str.slice(str.indexOf("fragment"));
-  
-    let fragName;
-  
-    const splitFrags = fragments.split("fragment");
-    console.log(splitFrags);
-    splitFrags.forEach((line) => {
-      if (line === "") return;
-  
-      //to find the index of the  '{' '}', and store it to bracksCache
-      let bracksCache = {};
-      for (let i = 0; i < line.length; i++) {
-        if (line[i] === "{" || line[i] === "}") {
-          if (!bracksCache[i]) bracksCache[i] = i;
-        }
+const calcCost = (str: string, depthFactor: number, costLimit: number) => {
+
+  //seperate the queries and fragments into different string
+  let query = str.slice(0, str.indexOf('fragment'));
+  let fragments = str.slice(str.indexOf('fragment'));
+
+  // if there more than one fragments, seperate them and store them into an array
+  const fragsArr = fragments.split('fragment');
+
+  let costSum = 0,
+    depthLvl = 0,
+    withinLimit = false;
+  let fragName: string;
+
+  const split = query.split('\n');
+ 
+  for (let i = 0; i < split.length; i++) {
+    if (split[i] === '') {
+      continue;
+    } else if (split[i].includes('{')) {
+      depthLvl += 1;
+    } else if (
+      !split[i].includes('{') &&
+      !split[i].includes('}') &&
+      !split[i].includes('...') &&
+      // includes any char other than white space
+      split[i].trim().length !== 0
+    ) {
+      if (depthLvl === 1) {
+        costSum += 1;
+      } else {
+        costSum += 1 * depthLvl * depthFactor;
       }
-      console.log(bracksCache);
-      const bracksIndices: number[] = Object.values(bracksCache);
-      console.log(bracksIndices);
-      firstBracket = Math.min(...bracksIndices);
-      lastBracket = Math.max(...bracksIndices);
-      console.log(firstBracket, lastBracket);
-      let fragmentName = line.slice(1, 5);
-      console.log(fragmentName);
-      if (!fragsObj[fragmentName])
-        fragsObj[fragmentName] = line.slice(firstBracket + 1, lastBracket);
-    });
-  
-    console.log(fragsObj);
-  
-    const splitQuery = query.split("\n");
-    console.log(splitQuery)
-    splitQuery.forEach((line, index) => {
-      if (line.includes("...")) {
-        let currIndex = line.indexOf(".");
-        fragName = line.slice(currIndex + 3, currIndex + 7);
-        console.log(fragName);
-        for (let i = 0; i < splitFrags.length; i++) {
-          if (splitFrags[i] !== "") {
-            if (splitFrags[i].includes(fragName)) {
-              splitQuery[index] = fragsObj[fragName];
+    } else if (split[i].includes('}')) {
+      depthLvl -= 1;
+    } else if (split[i].includes('...')) {
+      //find the current index of '.
+      let currIndex = split[i].indexOf('.');
+      fragName = split[i].slice(currIndex + 3);
+
+      // helper function to calculate the cost of the fragment
+      const calcFragCost = (fragNameStr) => {
+        let fragSum = 0;
+        let fragDepthLvl = depthLvl;
+
+        //loop throught the fragsArr, and and find the frag that matches the current fragment
+        for (let i = 0; i < fragsArr.length; i++) {
+          if (fragsArr[i].includes(fragNameStr)) {
+        
+            const current = fragsArr[i].split('\n');
+
+            //loop through current fragment, and calculate the cost the current fragment
+            for (let i = 1; i < current.length - 1; i++) {
+              console.log(current[i]);
+              if (current[i].includes('{')) {
+                fragDepthLvl++;
+              } else if (current[i].includes('}')) {
+                fragDepthLvl--;
+              } else if (
+                !current[i].includes('{') &&
+                !current[i].includes('}') &&
+                // includes any char other than white space
+                current[i].trim().length !== 0
+              ) {
+                if (current[i] === ' ') return;
+                // calculate the sum of the current fragment
+                fragSum += fragDepthLvl * depthFactor;
+
+              }
             }
           }
         }
-      }
-    });
-    console.log(splitQuery);
-    let mapped = splitQuery.map(el => {
-      if (el.includes('\n')) {
-        el = el.split('\n')
-        console.log(el)
-        return el
-      }
-      console.log(el)
-      return el
-    })
-    mapped = mapped.flat(Infinity)
-  
-    console.log(mapped)
-  
-    // const totalQuery = splitQuery.join("");
-    // console.log(totalQuery);
-    // return totalQuery;
-    return mapped
-  };
-  
-  const calcCostReal = (split, depthFactor, costLimit) => {
-    let costSum: number = 0,
-      depthLvl: number = 1,
-      withinLimit: Boolean;
-    // const split = query.split("\n");
-    for (let i = 1; i < split.length - 1; i++) {
-      if (split[i] === "") {
-        continue;
-      } else if (split[i].includes("{")) {
-        depthLvl += 1;
-      } else if (!split[i].includes("{") && !split[i].includes("}") ) {
-        if (depthLvl === 1) costSum += 1;
-        else {
-            console.log(costSum)  //1,7,11.5 ,17.5 ,23.5, 29.5
-          costSum += 1 * depthLvl * depthFactor;
-          console.log(costSum) 
-        }
-      } else if (split[i].includes("}")) {
-        depthLvl -= 1;
-      }
+        // add the sum of current fragment to the total cost 
+        costSum += fragSum;
+      };
+      calcFragCost(fragName);
     }
-    // check for whether the query is within the limit
-    costSum < costLimit ? (withinLimit = true) : (withinLimit = false);
-    return {
-      costSum: costSum,
-      withinLimit: withinLimit,
-    };
-  };
-  
-  console.log(calcCostReal(calcCost2(testStr), 1.5, 100));
-//   console.log(calcCostReal(testStr1, 1.5, 100));
+  }
+  // if sum of the total cost is smaller than the costLimit, return true. if not, return false
+  costSum < costLimit ? (withinLimit = true) : (withinLimit = false);
+  return { costSum: costSum, withinLimit: withinLimit }
+};
--- algo/depth.js
+++ algo/depth.js
@@ -18,49 +18,65 @@ const testStr = `{
   `
 
 
-export const depthLimit = (str: string, maxDepth: number): boolean => {
-  let query = str.slice(0, str.indexOf("fragment"));
-  let fragments = str.slice(str.indexOf("fragment"));
-  const fragDepths = {};
-
-  const fragsArr: string[] = fragments.split("fragment");
-
-  let targetFrag: string[] = [];
-
-  const calcDepth = (string: string) => {
-    let maxDepthSoFar: number = 0;
-    let currDepth: number = 0;
-    let fragName: string;
-    for (let i = 0; i < string.length; i++) {
-      if (string[i] === "{") {
-        currDepth += 1;
-      } else if (string[i] === "}") {
-        maxDepthSoFar = Math.max(currDepth, maxDepthSoFar);
-        currDepth -= 1;
-      } else if (string[i] === "." && string[i + 1] !== ".") {
-        fragName = string.slice(i + 1, i + 3);
-
-        let fragVal;
-        for (let key in fragDepths) {
-          if (key.includes(fragName)) {
-            fragVal = fragDepths[key];
+ const depthLimit = (str: string, maxDepth: number): any => {
+    let withinLimit: boolean;
+    //seperate the queries and fragments into different string
+    let query = str.slice(0, str.indexOf("fragment"));
+    let fragments = str.slice(str.indexOf("fragment"));
+    const fragDepths = {};
+  
+    const fragsArr: string[] = fragments.split("fragment");
+  
+    //helper function to calculate the depth of the fragment or the query
+    const calcDepth = (string: string) => {
+  
+      let maxDepthSoFar: number = 0;
+      let currDepth: number = 0;
+      let fragName: string;
+  
+      //loop through the string and find the max depth
+      for (let i = 0; i < string.length; i++) {
+        // if find the opening bracket, increase the current depth by 1
+        if (string[i] === "{") {
+          currDepth += 1;
+  
+          // if find the closing bracket, find the max depth so far by using Math.max and decrease the current depth by 1
+        } else if (string[i] === "}") {
+          maxDepthSoFar = Math.max(currDepth, maxDepthSoFar);
+          currDepth -= 1;
+  
+          // if find the "." , means this query has fragment
+        } else if (string[i] === "." && string[i + 1] !== ".") {
+          // find the name of this fragment
+          fragName = string.slice(i + 1, i + 3);
+  
+          let fragVal;
+          // find the depth of the fragment 
+          for (let key in fragDepths) {
+            if (key.includes(fragName)) {
+              fragVal = fragDepths[key];
+            }
           }
+          // add the current depth of the fragment to the curent depth
+          currDepth += fragVal;
+          // math.max the max Depth to find the max depth for now
+          maxDepthSoFar = Math.max(currDepth, maxDepthSoFar);
+          // remove the depth of the current fragment from the current depth
+          currDepth -= fragVal;
         }
-        currDepth += fragVal;
-        maxDepthSoFar = Math.max(currDepth, maxDepthSoFar);
-        currDepth -= fragVal;
       }
-    }
-    return maxDepthSoFar;
+      // return the max depth
+      return maxDepthSoFar;
+    };
+  
+    // store the name of fragments and the depth level of the fragment into object fragDepth in key-value paires
+    fragsArr.forEach((frag) => {
+      if (frag !== "") {
+        fragDepths[frag] = calcDepth(frag);
+      }
+    });
+    // if the total depth of the query is smaller than the given maxDepth, return true.
+    calcDepth(query) > maxDepth ? (withinLimit = false) : (withinLimit = true);
+    return { withinLimit: withinLimit, depth: calcDepth(query) };
   };
-
-  fragsArr.forEach((frag) => {
-    if (frag !== "") {
-      targetFrag.push(frag);
-      fragDepths[frag] = calcDepth(frag);
-    }
-  });
-
-  if (calcDepth(query) > maxDepth) return false;
-  else return true;
-};
+  
